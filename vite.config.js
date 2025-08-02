367eefd0fec4a98f66a2cf6caf9a558d0eac81aa
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2025-08-02 09:58:28 +0100

    deploy ready

---
+++ vite.config.js
@@ -0,0 +1,53 @@
+import { defineConfig } from "vite";
+import { createVuePlugin } from "vite-plugin-vue2";
+import path from "path";
+
+export default defineConfig({
+  base: "/stockmanagerb/",
+
+  plugins: [createVuePlugin()],
+
+  server: {
+    port: 3000,
+  },
+
+  optimizeDeps: {
+    include: [
+      "jquery",
+      "vue-loading-overlay",
+      "vue-progressbar",
+      "vue-toastr",
+      "vue-sweetalert2",
+      "vue-session",
+      "vue-cookies",
+    ],
+    exclude: [
+      "uglify-js",
+      "laravel-mix",
+      "terser-webpack-plugin",
+      "@swc/core",
+      "esbuild",
+    ],
+  },
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      external: [
+        "uglify-js",
+        "terser-webpack-plugin",
+        "laravel-mix",
+        "@swc/core",
+        "esbuild",
+      ],
+    },
+    sourcemap: false,
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      jquery: "jquery/dist/jquery.js",
+    },
+  },
+});
